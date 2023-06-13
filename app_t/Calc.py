e69f69004ffa06440f591a20d2e85a9db6a728d9
M.Kh <sea-of-max@yandex.ru>
2023-06-13 19:08:02 +0300

    New and Updated@

---
+++ app_t/Calc.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def multiply_(self, x, y):
+        return x * y
+    def division(self, x, y):
+        return x / y
+    def subtraction(self, x, y):
+        return x - y
+    def adding(self, x, y):
+        return x + y
---
+++ tests/test1.py
@@ -0,0 +1,25 @@
+import pytest
+from app_t.Calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+    def test_adding_negative(self):
+        assert self.calc.adding(self, 1, 1) == 3
+
+    def test_multiply(self):
+        assert self.calc.multiply_(self, 2,2) == 4
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 5, 3) == 2
+
+    def test_division_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+
+    def teardown(self):
+        print('Running method Teardown')
